76aeb8bb420d9c3e56ff587965ba7e9f15592f2f
Daniel Bercea <128615941+didocs@users.noreply.github.com>
2023-09-09 11:40:42 +0300

    Update scriptHangman.js

--- scriptHangman.js
+++ scriptHangman.js
@@ -1,28 +1,29 @@
-let wordList = ["aparataj", "dulap", "furgoneta", "salupa", "temperament", "timpan", "crocodil","spanzuratoare", "folositor", "curcubeu"];
+let wordList = ["aparataj", "dulap", "furgoneta", "salupa", "temperament", "timpan", "crocodil", "spanzuratoare", "folositor", "curcubeu"];
 
-let  gameWord, playerLife = 7, lifeStatus = document.getElementById("life-status");
+let gameWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+let playerLife = 7,
+    inputsArray = [],
+    pressedKeys = [],
+    letterCount = [],
+    foundLetters = 0,
+    lifeStatus = document.getElementById("life-status"),
+    messageBox = document.getElementById("message");
 
 lifeStatus.textContent = `Player life: ${playerLife}`;
 
 generateKeyboard();
 
-//fetch('wordsforHangman.txt')
-  //.then(response => response.text())
-  //.then(line => {
-    //wordList = line.split('\n').map(word => word.trim());
-    //newGame(); 
-  //})
-  //.catch(error => console.error('Error fetching words:', error));
-
 newGame();
 
 function newGame() {
-    gameWord = wordList[Math.floor(Math.random() * wordList.length)];
     for (let i = 0; i < gameWord.length; ++i) {
         let newInput = document.createElement("input");
         newInput.id = i;
         newInput.classList.add("password");
         document.getElementById("letters").appendChild(newInput);
+        inputsArray.push(newInput);
+        letterCount.includes(gameWord[i]) ? true : letterCount.push(gameWord[i]);
     }
 }
 
@@ -40,49 +41,29 @@ function generateKeyboard() {
 }
 
 function findLetter(id) {
-    let button = document.getElementById(id);
     let match = gameWord.search(id);
-    if (match === -1) {
+    let button = document.getElementById(id);
+    if (match === -1 && !pressedKeys.includes(id)) {
+        --playerLife;
         button.style.backgroundColor = "red";
-        notFound();
-    } else {
-  		  showLetters(id);
-        button.style.backgroundColor = "lightgreen";
-    }
-}
-
-function showLetters(id) {
-	  let remainingLetters = 0;
-    for (let i = 0; i < gameWord.length; ++i) {
-        let textBox = document.getElementById(i);
-        if (id === gameWord[i]) {
-            textBox.placeholder = id;
-        } 
-        if (textBox.placeholder === "") {
-        	  ++remainingLetters;
-            console.log(remainingLetters);
-		    }
-    }
-    if (remainingLetters === 0 && playerLife > 0) {
-    	 document.getElementById("message").innerHTML = "Congratulations!!:) You found the word!";
-    }
-}
-
-function notFound() {
-    --playerLife;
-    if (playerLife > 0) {
         lifeStatus.textContent = `Player life: ${playerLife}`;
-    } else {
-        lifeStatus.textContent = "";
-        for (let i = 0; i < gameWord.length; ++i) {
-        	showLetters(gameWord[i]);
+        if (playerLife === 0) {
+            inputsArray.forEach(input => input.placeholder = gameWord[input.id]);
+            messageBox.innerText = "Game Over:( You were Hanged!";
+            let buttons = document.querySelectorAll("#keyboard button");
+            buttons.forEach(function(button) {
+                button.parentNode.removeChild(button);
+            });
         }
-        document.getElementById("message").innerHTML = "Game Over:( You were Hanged!";
-        let buttons = document.querySelectorAll("#keyboard button");
-        buttons.forEach(function(button) {
-            button.parentNode.removeChild(button);
+    } else if (match != -1 && !pressedKeys.includes(id)) {
+        ++foundLetters;
+        button.style.backgroundColor = "lightgreen";
+        gameWord.split('').forEach((letter, i) => {
+            gameWord[i] === id ? inputsArray[i].placeholder = id : null;
         });
+        foundLetters === letterCount.length ? messageBox.innerText = "Congratulations!!:) You found the word!" : null;
     }
+    pressedKeys.includes(id) ? true : pressedKeys.push(id);
 }
 
 function restart() {
